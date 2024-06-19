446853b9bc77201e19c5d2b455932edc465b2ba9
eunmmm <rosiebaek223@gmail.com>
2024-06-20 00:33:43 +0900

    Feat: [T-02] Firebase Realtime Database 설정 및 데이터 읽기/쓰기 기능 테스트
    - setDatabase 함수로 데이터베이스에 데이터를 설정하는 기능 테스트
    - getData 함수로 데이터베이스에서 데이터를 읽어오는 기능 테스트
    - 폼 제출 시 데이터베이스에 값을 저장하는 이벤트 리스너 테스트
    
    https://www.notion.so/eunmi/03-firebase-b4d519fd6aff4b568d0561541aebd113

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set, onValue } from 'firebase/database';
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -9,4 +12,33 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-export default firebaseConfig;
+const app = initializeApp(firebaseConfig);
+const database = getDatabase();
+
+function setDatabase(collectionName, testContent) {
+  const db = getDatabase();
+  set(ref(db, `tests/${collectionName}`), {
+    testContent,
+  });
+}
+
+function getData(testId) {
+  const databaseRef = ref(database, `tests/${testId}`);
+  onValue(databaseRef, (snapshot) => {
+    const data = snapshot.val();
+    console.log(data);
+  });
+}
+
+getData('test');
+
+const form = document.querySelector('#testForm');
+const input = document.querySelector('#testInput');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  setDatabase('test', input.value);
+  input.value = '';
+});
+
+export default app;
