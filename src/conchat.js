a3ccbfbdca3d9d49c456050bd87f5191a8fb8e87
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-06-20 03:32:36 +0900

    Feat: [T-03] con.chat() 메서드 구현
    - 콘솔 채팅 기능 활성화
    - 안내 메시지 출력
    
    https://www.notion.so/eunmi/T-03-con-chat-5a50787f365c4005aeb6864110675107

---
+++ src/conchat.js
@@ -0,0 +1,18 @@
+class Con {
+  #state = false;
+
+  #setState(boolean) {
+    this.#state = boolean;
+  }
+
+  chat() {
+    this.#setState(true);
+    console.log(
+      '🌽conchat을 시작합니다!\n\n우리는 JavaScript와 React 환경에서 채팅이 가능합니다.\n1. JavaScript\n2. React\n어떤 언어를 사용하고 있나요? con.setLanguage("js" 또는 "react")를 입력해주세요!'
+    );
+  }
+}
+
+window.con = new Con();
+
+export default window.con;
