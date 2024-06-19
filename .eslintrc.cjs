759332831b617af3bb9b59362713e72f4a90fc81
김정헌 <metronom7703@naver.com>
2024-06-20 00:19:48 +0900

    Setting: [T-01] npm 패키지 설정 및 개발 도구 추가
    - Cypress, ESLint, Prettier, Husky, lint-staged 등 개발 도구 추가
    - Vite 및 Vitest 설정
    - Firebase 패키지 추가
    
    https://eunmi.notion.site/01-e56398abe98948509feabc22733f3b0e?pvs=4

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': ['error', 'ignorePackages'],
+  },
+};
