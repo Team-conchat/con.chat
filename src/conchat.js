c3b069d605f04a3abc74ac1b64a2dd04784ba357
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-06-20 14:20:27 +0900

    Refactor: [T-03] trailingComma 적용
    - 개행 처리된 함수 인자에 trailingComma 적용
    
    https://www.notion.so/eunmi/T-03-con-chat-5a50787f365c4005aeb6864110675107

--- src/conchat.js
+++ src/conchat.js
@@ -8,7 +8,7 @@ class Con {
   chat() {
     this.#setState(true);
     console.log(
-      '🌽conchat을 시작합니다!\n\n우리는 JavaScript와 React 환경에서 채팅이 가능합니다.\n1. JavaScript\n2. React\n어떤 언어를 사용하고 있나요? con.setLanguage("js" 또는 "react")를 입력해주세요!'
+      '🌽conchat을 시작합니다!\n\n우리는 JavaScript와 React 환경에서 채팅이 가능합니다.\n1. JavaScript\n2. React\n어떤 언어를 사용하고 있나요? con.setLanguage("js" 또는 "react")를 입력해주세요!',
     );
   }
 }
