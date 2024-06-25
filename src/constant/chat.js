c913f7c1356ad473f8fd7775bc1cf02995ef506a
eunmmm <rosiebaek223@gmail.com>
2024-06-25 20:34:46 +0900

    Refactor: [T-16] 상수 스타일 추가 및 변수명 변경
    - 메시지 표시용 스타일 'TEXT_BLOCK_STYLE'을 추가
    - 변수 'ix'를 'index'로 변경
    
    https://debonair-bread-c72.notion.site/T-16-con-insertElement-4b413e16440b43238bec7c67543ce9c3

--- src/constant/chat.js
+++ src/constant/chat.js
@@ -3,4 +3,7 @@ const DEFAULT_USER_NAME = '아무개';
 const CODE_BLOCK_STYLE =
   'padding: 3px; border-radius: 3px; background-color: #ededeb; color: #37352f;';
 
-export { DEFAULT_USER_NAME, CODE_BLOCK_STYLE };
+const TEXT_BLOCK_STYLE =
+  'padding: 3px; border-radius: 3px; background-color: #fbfb87; color: #37352f;';
+
+export { DEFAULT_USER_NAME, CODE_BLOCK_STYLE, TEXT_BLOCK_STYLE };
