393c13a9f153281f7c110fdce2528dfb4dcc0f5b
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-06-28 22:41:39 +0900

    Feat: [T-22] con.removeElement() 구현
    - 설정한 element를 삭제하는 기능 구현
    - removeElement 리스너 타입 분기
    
    https://www.notion.so/T-22-con-removeElement-6f5b63c8d607403180c8249a87632be0?pvs=4

--- src/constant/chat.js
+++ src/constant/chat.js
@@ -2,31 +2,31 @@ const PUBLIC_ROOM_KEY = 'public';
 const DEFAULT_USER_NAME = '아무개';
 
 const CODE_BLOCK_STYLE = `
-  padding: 3px;
-  border-radius: 3px;
+  padding: 3px 5px;
+  border-radius: 4px;
   background-color: #ededeb;
   color: #37352f;
 `;
 
 const TEXT_BLOCK_STYLE = `
-  padding: 3px;
-  border-radius: 3px;
+  padding: 3px 5px;
+  border-radius: 4px;
   background-color: #fbfb87;
   color: #37352f;
 `;
 
 const COMPONENT_BLOCK_STYLE = `
+  padding: 3px 5px;
+  border-radius: 4px;
   background-color: #F2CF65;
   color: #000;
-  padding: 3px 5px;
-  border - radius: 4px;
 `;
 
 const ROOT_COMPONENT_BLOCK_STYLE = `
-  background-color: #96D259;
-  color: #000;
   padding: 3px 5px;
   border-radius: 4px;
+  background-color: #96D259;
+  color: #000;
 `;
 
 export {
