62eea25b6851f61702210c88f9b61a67ef4389c2
eunmmm <rosiebaek223@gmail.com>
2024-08-05 14:42:25 +0900

    Fix: deploy-script 순환 복사 문제 해결

--- deploy-script.js
+++ deploy-script.js
@@ -1,5 +1,6 @@
 import fs from 'fs-extra';
 import { readFile } from 'fs/promises';
+import path from 'path';
 
 const packageJson = JSON.parse(
   await readFile(new URL('./package.json', import.meta.url)),
@@ -7,10 +8,23 @@ const packageJson = JSON.parse(
 const { version } = packageJson;
 
 const sourceDir = 'dist';
+const tempDir = `temp_v${version}`;
 const targetDir = `dist/v${version}`;
+
+if (fs.existsSync(tempDir)) {
+  fs.removeSync(tempDir);
+}
+fs.copySync(sourceDir, tempDir);
 
 if (fs.existsSync(targetDir)) {
   fs.removeSync(targetDir);
 }
 
-fs.copySync(sourceDir, targetDir, { overwrite: true });
+fs.moveSync(tempDir, targetDir);
+
+const distContents = fs.readdirSync(sourceDir);
+distContents.forEach((item) => {
+  if (item !== `v${version}`) {
+    fs.removeSync(path.join(sourceDir, item));
+  }
+});
